eee96a20b2fdc91de6e8b8776401f3849e1ee88f
Stephan Dilly <dilly.stephan@gmail.com>
2020-11-10 14:00:49 +0100

    cleanup

--- godot/GDBench.gd
+++ godot/GDBench.gd
@@ -1,18 +1,23 @@
 extends CanvasItem
 
 export var cnt = 4000
+export var start = Vector2(250,250)
+export var rad = 200
 
 func _draw():
 	var startTime = OS.get_ticks_usec()
-	var start = Vector2(200,200)
 
 	var cntf = float(cnt)
-	var rad = 200
 	for n in range(cnt):
 		var x = sin(n/cntf * 360.0)*rad
 		var y = cos(n/cntf * 360.0)*rad
-		draw_line(start, start+Vector2(x, y), Color(255, 0, 0), 1,false)
-	
+		draw_line(
+			start, 
+			start+Vector2(x, y), 
+			Color(255, 0, 0), 
+			1,
+			false)
+
 	print("bench: " + String(OS.get_ticks_usec() - startTime))
 
 func _process(_delta):
